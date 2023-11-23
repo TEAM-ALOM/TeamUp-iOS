40dae2e9302a869efb8e191ca0a80a84c571b245
SeoU <tjdnfkd1225@naver.com>
2023-11-23 18:49:24 +0900

    Setting Folder Convention

---
+++ TeamUP/TeamUP/App/TeamUPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamUPApp.swift
+//  TeamUP
+//
+//  Created by 은서우 on 2023/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TeamUPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
