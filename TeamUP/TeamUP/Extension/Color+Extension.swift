43507753a35909a55be6197d2fd194f0868987d7
SeoU <tjdnfkd1225@naver.com>
2023-12-07 16:46:10 +0900

    asset 및 color 추가
    - LogoColor 추가
    - icon 추가

--- TeamUP/TeamUP/Extension/Color+Extension.swift
+++ TeamUP/TeamUP/Extension/Color+Extension.swift
@@ -15,4 +15,5 @@ extension Color {
     static let customSkyblue = Color("Skyblue")
     static let customGray01 = Color("Gray01")
     static let customGray02 = Color("Gray02")
+    static let LogoColor = Color("LogoColor")
 }
