1085929b388a5ff763368799680f250740b463a5
SeoU <tjdnfkd1225@naver.com>
2023-12-29 17:31:41 +0900

    Date+Extension 추가

--- TeamUP/TeamUP/Extension/Date+Extension.swift
+++ TeamUP/TeamUP/Extension/Date+Extension.swift
@@ -11,7 +11,25 @@ extension Date {
     var formattedTime: String {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ko_KR")
-        formatter.dateFormat = "a hh:mm"
+        formatter.dateFormat = "a hh:mm" // 오전/오후 시간:분
         return formatter.string(from: self)
+    }
+    
+    var formattedDay: String {
+      let now = Date()
+      let calendar = Calendar.current
+      
+      let nowStartOfDay = calendar.startOfDay(for: now)
+      let dateStartOfday = calendar.startOfDay(for: self)
+      let numOfDifference = calendar.dateComponents([.day], from: nowStartOfDay, to: dateStartOfday).day!
+      
+      if numOfDifference == 0 {
+        return "오늘"
+      } else {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "M월 d일 E요일"
+        return formatter.string(from: self)
+      }
     }
 }
